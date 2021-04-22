057bc9555555426f3cddf73afbc34bb0e5356ff3
Masonstorm1337 <80617604+Masonstorm1337@users.noreply.github.com>
2021-04-22 12:03:15 -0400

    Raw v1
    works

--- Console-pong/Console-pong.cpp
+++ Console-pong/Console-pong.cpp
@@ -13,8 +13,10 @@ int nScreenHeight = 30;
 
 
 int playerA = 15;
-int playerB = 20;
+int playerB = 15;
 int score = 0;
+int ai;
+int flatRander;
 int projectialA[4] = { nScreenWidth / 2 , 15, 0, 0};
 bool bKeyUp = false;
 bool bKeyDown = false;
@@ -52,12 +54,25 @@ int main()
         if (playerA < 2) playerA = 2;
         if (playerA > nScreenHeight - 5) playerA--;
 
-        //Clear moved location
+        // AI
+        
+        ai = rand() % 10 + 1;
+        if (ai > 7) {
+            if (projectialA[1] > playerB) {
+                playerB++;
+            }
+            else playerB--;
 
+
+        }
+                
+
+        //Clear moved location
         for (int x = 2; x < nScreenHeight - 2; x++) {
             screen[nScreenWidth * x + 4] = L' ';
             screen[nScreenWidth * x + nScreenWidth - 6 ] = L' ';
         }
+
         screen[ projectialA[0] + projectialA[1] * nScreenWidth] =L' ';
         projectialA[3] = projectialA[2];
         
@@ -91,33 +106,133 @@ int main()
 
         
         }
+        
         for (int x = 0; x < 3; x++) {
             screen[nScreenWidth * (playerA + x) + 4] = L'l';
             screen[nScreenWidth * (playerB + x) + nScreenWidth - 6] = L'l';
         }
 
-        if (screen[projectialA[0] + projectialA[1] * nScreenWidth] == L'X' || screen[projectialA[0] + projectialA[1] * nScreenWidth] == L'l')
-        {
+        if (screen[projectialA[0] + projectialA[1] * nScreenWidth] == L'X'){
             if (projectialA[0] < 2) {
                 score--; 
                 projectialA[0] = nScreenWidth / 2;
                 projectialA[1] = 15;
                 projectialA[2] = rand() % 6 + 1 ;
-            }
-            if (projectialA[0] > nScreenWidth -3) {
+                projectialA[3] = projectialA[2];
+            } else if (projectialA[0] > nScreenWidth -3) {
                 score++;
                 projectialA[0] = nScreenWidth / 2;
                 projectialA[1] = 15;
                 projectialA[2] = rand() % 6 + 1;
+                projectialA[3] = projectialA[2];
+                
             }
-
-            projectialA[0] = nScreenWidth / 2;
-            projectialA[1] = 15;
-            projectialA[2] = rand() % 6 + 1;
-
-
-
+            else {
+                switch (projectialA[3]) {
+                case 1:
+                    projectialA[0] = projectialA[0] - 1;
+                    projectialA[1] = projectialA[1] + 0;
+                    flatRander = rand() % 3;
+                    switch (flatRander) {
+                    case 1:
+                        projectialA[2] = 4;
+                        break;
+                    case 2:
+                        projectialA[2] = 5;
+                        break;
+                    case 3:
+                        projectialA[2] = 6;
+                        break;
+                    }
+                    break;
+                case 2:
+                    projectialA[0] = projectialA[0] - 1;
+                    projectialA[1] = projectialA[1] - 1;
+                    projectialA[2] = 3;
+                    break;
+                case 3:
+                    projectialA[0] = projectialA[0] - 1;
+                    projectialA[1] = projectialA[1] + 1;
+                    projectialA[2] = 2;
+                    break;
+                case 4:
+                    projectialA[0] = projectialA[0] + 1;
+                    projectialA[1] = projectialA[1] - 0;
+                    flatRander = rand() % 3;
+                    switch (flatRander) {
+                    case 1:
+                        projectialA[2] = 1;
+                        break;
+                    case 2:
+                        projectialA[2] = 2;
+                        break;
+                    case 3:
+                        projectialA[2] = 3;
+                        break;
+                    }
+                    break;
+                case 5:
+                    projectialA[0] = projectialA[0] + 1;
+                    projectialA[1] = projectialA[1] - 1;
+                    projectialA[2] = 6;
+                    break;
+                case 6:
+                    projectialA[0] = projectialA[0] + 1;
+                    projectialA[1] = projectialA[1] + 1;
+                    projectialA[2] = 5;
+                    break;
+                }
+            }
+                
         }
+
+        if (screen[projectialA[0] + projectialA[1] * nScreenWidth] == L'l')
+        {
+            if (projectialA[0] >  5) {
+                projectialA[0] = projectialA[0] - 10;
+                projectialA[1] = projectialA[1] + 0;
+                flatRander = rand() % 3 +1;
+                    switch (flatRander) {
+                    case 1:
+                        projectialA[2] = 4;
+                        break;
+                    case 2:
+                        projectialA[2] = 5;
+                        break;
+                    case 3:
+                        projectialA[2] = 6;
+                        break;
+                        projectialA[3] = projectialA[2];
+                    }
+            }
+            if (projectialA[0] < 5) 
+            {
+                projectialA[0] = projectialA[0] + 1;
+                projectialA[1] = projectialA[1] + 0;
+                flatRander = rand() % 3 + 1;
+                    switch (flatRander)
+                    {
+                    case 1:
+                        projectialA[2] = 1;
+                        break;
+                    case 2:
+                        projectialA[2] = 2;
+                        break;
+                    case 3:
+                        projectialA[2] = 3;
+                        break;
+                        projectialA[3] = projectialA[2];
+                    }
+            }
+            }
+        
+            //projectialA[0] = nScreenWidth / 2;
+            //projectialA[1] = 15;
+            //projectialA[2] = rand() % 6 + 1;
+
+
+
+        
         screen[projectialA[0] + projectialA[1] * nScreenWidth] = L'O';
 
 
@@ -127,6 +242,6 @@ int main()
         WriteConsoleOutputCharacter(hConsole, screen , nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
         
     }
-  
-}
+  }
+
 
